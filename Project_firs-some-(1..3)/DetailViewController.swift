a6add76ce3cd5ba1a0dfe5f36b58125bfcfcf042
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-10-24 19:30:55 +0300

    add_new_project-fix

--- Project_firs-some-(1..3)/DetailViewController.swift
+++ Project_firs-some-(1..3)/DetailViewController.swift
@@ -8,22 +8,53 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+//    var selectedPictureNumber = 0
+//    var totalPictures = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Гля який флажок"
+        //"This image is \(selectedPictureNumber) from \(totalPictures)"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        navigationItem.largeTitleDisplayMode = .never
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
+        }      
+        
     }
-    
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            navigationController?.hidesBarsOnTap = true
+        }
+        override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            navigationController?.hidesBarsOnTap = false
+        }
+        @objc func shareTapped() {
+            guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+                print("No image found")
+                return
+            }
+            let imageName = selectedImage ?? "unknown_image.jpg"
+            // Создайте временный файл
+                let temporaryDirectory = FileManager.default.temporaryDirectory
+                let fileURL = temporaryDirectory.appendingPathComponent(imageName)
 
-    /*
-    // MARK: - Navigation
+                do {
+                    // Запишите данные изображения в файл
+                    try image.write(to: fileURL)
+                    
+                    // Передаем файл вместо данных изображения
+                    let itemsToShare: [Any] = [fileURL]
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+                    let vc = UIActivityViewController(activityItems: itemsToShare, applicationActivities: nil)
+                    vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+                    present(vc, animated: true)
+                } catch {
+                    print("Error saving image: \(error)")
+                }
     }
-    */
-
 }
--- Project_firs-some-(1..3)/ViewController.swift
+++ Project_firs-some-(1..3)/ViewController.swift
@@ -7,13 +7,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Установите шрифт для большого заголовка
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 28)] // Размер шрифта для большого заголовка
+        title = "Диви які гарні флажки"
+        //navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Добавляем кнопку "Назад" слева
+        let backButton = UIBarButtonItem(title:"\u{274C}", style: .plain, target: self, action: #selector(backButtonTapped))
+        navigationItem.leftBarButtonItem = backButton
+        
+        setupRecommendButton()
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        
+        for item in items {
+            if item.hasSuffix("3x.png") {
+                //                let trimmedName = item.replacingOccurrences(of: "@3x.png", with: "")
+                //                let uppercasedName = trimmedName.uppercased()
+                //                pictures.append(uppercasedName)
+                pictures.append(item)
+                pictures.sort()
+                print(pictures)
+            }
+        }
     }
-
-
+    
+    func setupRecommendButton() {
+        // Создание кнопки "Рекомендовать приложение"
+        let recommendButton = UIBarButtonItem(title: "\u{1F31F}", style: .plain, target: self, action: #selector(recommendTapped))
+        navigationItem.rightBarButtonItem = recommendButton
+    }
+    
+    @objc func recommendTapped() {
+        let appURL = URL(string: "https://github.com/Prysiazhnyi?tab=repositories")!
+        let shareText = "https://github.com/Prysiazhnyi?tab=repositories"
+        
+        let itemsToShare = [shareText, appURL] as [Any]
+        
+        let activityVC = UIActivityViewController(activityItems: itemsToShare, applicationActivities: nil)
+        activityVC.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem // Настройка для iPad
+        present(activityVC, animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        
+        // Убираем суффикс и преобразуем в верхний регистр для отображения
+        let pictureName = pictures[indexPath.row]
+        let trimmedName = pictureName.replacingOccurrences(of: "@3x.png", with: "")
+        let uppercasedName = trimmedName.uppercased()
+        
+        cell.textLabel?.text = uppercasedName
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            // 2: success! Set its selectedImage property
+            vc.selectedImage = pictures[indexPath.row]
+            
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    // Действие при нажатии на кнопку "Назад"
+    @objc func backButtonTapped() {
+        // Переводим приложение в фоновый режим
+        UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
+    }
 }
 
